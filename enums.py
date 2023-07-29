b2651af861b4a509515e252e02fe609f25e8777e
metabenic <metabenic@gmail.com>
2023-07-29 02:40:50 -0700

    refactor

--- enums.py
+++ enums.py
@@ -1,53 +1,48 @@
 from enum import Enum, IntEnum
 
+class LiveStatus(IntEnum):
+    """Enumeration that defines potential states of the live stream"""
+    BOT_INIT  = 0
     LAGGING   = 1
+    LIVE      = 2
+    OFFLINE   = 3
 
-class TimeOut(IntEnum):
-    """ 
-    Enumeration that defines timeout values. 
-    """
-    def __mul__(self, operator):
-        return self.value * operator
 
+class WaitTime(IntEnum):
+    """Enumeration that defines wait times in seconds."""
+    LONG  = 120
+    SHORT = 60
     LAG   = 5
 
 
 class StatusCode(IntEnum):
-    OK = 200
-    REDIRECT = 302
+    """Enumeration that defines HTTP status codes."""
+    OK          = 200
+    REDIRECT    = 302
+    BAD_REQUEST = 400
 
 
 class Mode(IntEnum):
-    """
-    Enumeration that represents the recording modes.
-    """
-    MANUAL = 0
+    """Enumeration that represents the recording modes."""
+    MANUAL    = 0
     AUTOMATIC = 1
 
 
-class Error(Enum):
-    """
-    Enumeration that contains possible errors while using TikTok-Live-Recorder.
-    """
+class ErrorMsg(Enum):
+    """Enumeration of error messages"""
     def __str__(self):
         return str(self.value)
 
-    AUTOMATIC_MODE_ERROR: str = "[-] Automatic mode can be used only in unblacklisted country. Use a VPN\n[*] " \
-                                "Unrestricted country list: " \
-                                "https://github.com/Michele0303/TikTok-Live-Recorder/edit/main/GUIDE.md#unrestricted" \
-                                "-country"
-
-    HTTP_ERROR = "[-] Captcha require or country blocked. Use a vpn or room_id." \
-                 "\n[-] How to get room id: https://github.com/Michele0303/TikTok-Live-Recorder/blob/main/GUIDE.md#how-to-get-room_id" \
-                 "\n[-] Unrestricted country list: https://github.com/Michele0303/TikTok-Live-Recorder/edit/main/GUIDE" \
-                 ".md#unrestricted-country"
-
-    USERNAME_ERROR = "[-] Error: Username/Room_id not found or the user has never been in live"
-
-    CONNECTION_CLOSED = "[-] Connection broken by the server."
-    CONNECTION_CLOSED_AUTOMATIC = f"{CONNECTION_CLOSED}. Try again after Delay of {TimeOut.CONNECTION_CLOSED} minutes"
-
-    URL_NOT_FOUND = "[-] Unable to find stream url."
+    BLKLSTD_AUTO_MODE_ERROR: str = 'Automatic mode can be used only in unblacklisted country. Use a VPN\n[*] ' \
+                                'Unrestricted country list: ' \
+                                'https://github.com/Michele0303/TikTok-Live-Recorder/edit/main/GUIDE.md#unrestricted' \
+                                '-country'
+    BLKLSTD_ERROR = 'Captcha required or country blocked. Use a vpn or room_id.' \
+                 '\nTo get room id: https://github.com/Michele0303/TikTok-Live-Recorder/blob/main/GUIDE.md#how-to-get-room_id' \
+                 '\nUnrestricted country list: https://github.com/Michele0303/TikTok-Live-Recorder/edit/main/GUIDE' \
+                 '.md#unrestricted-country'
+    USERNAME_ERROR = 'Error: Username/Room_id not found or the user has never been in live'
+    CONNECTION_CLOSED = 'Connection broken by the server.'
 
 
 class Info(Enum):
