075418ce2d6a77d1a00b2ad305d5da6d5efd25ae
metabenic <metabenic@gmail.com>
2023-11-11 07:54:54 -0800

    fix error

--- tiktok_bot.py
+++ tiktok_bot.py
@@ -247,14 +247,8 @@ class TikTok:
                 logging.info(f'Getting live url for room ID {self.room_id}')
             url = f'https://webcast.tiktok.com/webcast/room/info/?aid=1988&room_id={self.room_id}'
             json = self.req.get(url, headers=bot_utils.headers).json()
-            if bot_utils.check_exists(json, ['data', 'prompts']):
-                if 'This account is private' in json['data']['prompts']:
-                    if not self.browser_exec:
-                        raise errors.AccountPrivate('Account is private, login required')
-                    else:
-                        logging.info('Account is private, login required')
-                        browser_extractor = BrowserExtractor(self.room_id, self.browser_exec)
-                        return browser_extractor.get_live_url()
+                    browser_extractor = BrowserExtractor()
+                    return browser_extractor.get_live_url(self.room_id, self.browser_exec)
             if not bot_utils.check_exists(json, ['data', 'stream_url', 'rtmp_pull_url']):
                 raise ValueError(f'rtmp_pull_url not in response: {json}')
             return json['data']['stream_url']['rtmp_pull_url']
