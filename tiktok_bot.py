04d78cb9141f713d20bcf20760c665c55b1e0a63
metabenic <metabenic@gmail.com>
2023-11-11 07:44:22 -0800

    prevent ffmpeg from hanging on network outage

--- tiktok_bot.py
+++ tiktok_bot.py
@@ -151,7 +151,7 @@ class TikTok:
         """Show real-time stats and raise ffmpeg errors"""
         stream = ffmpeg.input(live_url, **{'loglevel': 'error'}, **{'reconnect': 1}, 
                               **{'reconnect_streamed': 1}, **{'reconnect_at_eof': 1},
-                              **{'reconnect_delay_max': 5}, stats=None)
+                              **{'reconnect_delay_max': 5}, **{'timeout': 10000000}, stats=None)
         stats_shown = False
         if self.duration is not None:
             stream = ffmpeg.output(stream, self.out_file, c='copy', t=self.duration)
